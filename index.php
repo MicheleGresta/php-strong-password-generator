a34065a81aa9f3ccba0bb2c71cf14be0c1423693
MicheleGresta <malachia_1997@hotmail.it>
2023-09-08 18:22:09 +0200

    pagina function.php

--- index.php
+++ index.php
@@ -1,38 +1,7 @@
 <?php
 
-session_start();
+include "./functions.php"
 
-$inputPassword = strlen($_GET["user-password"] ?? false);
-
-
-
-$simboli["minuscole"] = 'abcdefghijklmnopqrstuvwxyz';
-$simboli["maiuscole"] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-$simboli["numeri"] = '1234567890';
-$simboli["speciali"] = '!?~@#-_+<>[]{}';
-
-$arrayChars = "";
-
-$minuscole = $_GET['minuscole'] ?? false;
-$maiuscole = $_GET['maiuscole'] ?? false;
-$numeri = $_GET['numeri'] ?? false;
-$speciali = $_GET['speciali'] ?? false;
-
-if ($minuscole == true){
-    $arrayChars .= $simboli['minuscole'];
-}
-if ($maiuscole == true){
-    $arrayChars .= $simboli['maiuscole'];
-}
-if ($numeri == true){
-    $arrayChars .= $simboli['numeri'];
-}
-if ($speciali == true){
-    $arrayChars .= $simboli['speciali'];
-}
-
-$pwgen = substr(str_shuffle($arrayChars), 0, $inputPassword);
-$_SESSION['pwstamp'] = $pwgen;
 
 ?>
 <!DOCTYPE html>
