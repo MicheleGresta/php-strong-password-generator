54a8d279c701269b5961d3f7ee0e7ec0f8444fa9
MicheleGresta <malachia_1997@hotmail.it>
2023-09-08 17:55:30 +0200

    generatore in una pagina

---
+++ index.php
@@ -0,0 +1,87 @@
+<?php
+
+session_start();
+
+$inputPassword = strlen($_GET["user-password"] ?? false);
+
+
+
+$simboli["minuscole"] = 'abcdefghijklmnopqrstuvwxyz';
+$simboli["maiuscole"] = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+$simboli["numeri"] = '1234567890';
+$simboli["speciali"] = '!?~@#-_+<>[]{}';
+
+$arrayChars = "";
+
+$minuscole = $_GET['minuscole'] ?? false;
+$maiuscole = $_GET['maiuscole'] ?? false;
+$numeri = $_GET['numeri'] ?? false;
+$speciali = $_GET['speciali'] ?? false;
+
+if ($minuscole == true){
+    $arrayChars .= $simboli['minuscole'];
+}
+if ($maiuscole == true){
+    $arrayChars .= $simboli['maiuscole'];
+}
+if ($numeri == true){
+    $arrayChars .= $simboli['numeri'];
+}
+if ($speciali == true){
+    $arrayChars .= $simboli['speciali'];
+}
+
+$pwgen = substr(str_shuffle($arrayChars), 0, $inputPassword);
+$_SESSION['pwstamp'] = $pwgen;
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.3.1/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container py-5">
+
+        <form method="GET">
+            <input type="text" name="user-password" class="mb-5">
+
+            <button type="submit" class="btn btn-danger">Genera</button>
+            <div class="container-checks">
+                <div class="d-flex align-items-center gap-1">
+                    <input type="checkbox" checked name="minuscole"> <span><strong>Lettere minuscole</strong></span>
+                </div>
+                <div class="d-flex align-items-center gap-1">
+                    <input type="checkbox" name="maiuscole"> <span><strong>Lettere maiuscole</strong></span>
+                </div>
+                <div class="d-flex align-items-center gap-1">
+                    <input type="checkbox" name="numeri"> <span><strong>Numeri</strong></span>
+                </div>
+                <div class="d-flex align-items-center gap-1">
+                    <input type="checkbox" name="speciali"> <span><strong>Caratteri speciali</strong></span>
+                </div>
+
+
+
+            </div>
+            <div>
+                <h4 class="pt-5">Nuova password generata:</h4>
+                <h5 class="py-3"> <?php echo $pwgen ?></h5>
+            </div>
+
+
+        </form>
+    </div>
+
+</body>
+
+</html>
